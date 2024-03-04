aa087ba39137d66f0f79926c3b54fedd68940b86
Joshen96 <asdzxc250@naver.com>
2024-03-04 19:25:44 +0900

    실드시스템 추가 원거리 적 추가

---
+++ Source/TPSProject/public/MyEnemy_Throw.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "MyEnemy_Throw.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSPROJECT_API AMyEnemy_Throw : public AEnemy
+{
+	GENERATED_BODY()
+	
+
+	AMyEnemy_Throw();
+
+};
+
